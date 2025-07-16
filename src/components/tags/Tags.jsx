66b7d55d100bdd3eba6a92da2a5a7500ab9e631d
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-07-16 23:03:55 +0600

    tag are visible at UI

--- src/components/tags/Tags.jsx
+++ src/components/tags/Tags.jsx
@@ -1,11 +1,22 @@
 import Tag from "./Tag";
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTags } from "../../redux/features/tags/tagsSlice";
 export default function Tags() {
-    return (
-        <section>
-            <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
-                <Tag />
-            </div>
-        </section>
-    );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTags());
+  }, [dispatch]);
+
+  const { tags } = useSelector((state) => state.tags);
+  return tags?.length > 0 ? (
+    <section>
+      <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
+        {tags.map((tag) => (
+          <Tag key={tag?.id} tag={tag?.title} />
+        ))}
+      </div>
+    </section>
+  ) : null;
 }
